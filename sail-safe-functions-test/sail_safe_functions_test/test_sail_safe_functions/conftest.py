3d28e804616b46ebb972669450ac24110e07e15e
Jaap Oosterbroek <jaap@secureailabs.com>
2022-04-28 13:08:44 +0100

    adding implementation and tests

--- sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/conftest.py
+++ sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/conftest.py
@@ -5,14 +5,24 @@ import os
 from typing import Tuple
 
 import numpy as np
+import pandas as pd
 import pytest
 from config import DATA_PATH
-from sail_safe_functions_test.helper_sail_safe_functions.dataframe_federated_local import (
-    DataframeFederatedLocal,
-)
-from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import (
-    SeriesFederatedLocal,
-)
+from sail_safe_functions_test.helper_sail_safe_functions.dataframe_federated_local import DataframeFederatedLocal
+from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import SeriesFederatedLocal
+
+
+@pytest.fixture
+def dataframe_kidney() -> pd.DataFrame:
+    """
+    Fixture for loading a dataframe with some missing values
+
+    :return: dataframe_kidney: A dataframe with some missing fields
+    :rtype: class : pd.DataFrame
+    """
+
+    path_file_csv = os.path.join(DATA_PATH, "data_csv_kidney", "kidney_disease.csv")
+    return pd.read_csv(path_file_csv)
 
 
 @pytest.fixture
@@ -62,9 +72,7 @@ def two_sample_small() -> Tuple[SeriesFederatedLocal, SeriesFederatedLocal]:
     :return: SeriesFederatedLocal
     :rtype: class : test_sail_safe_functions.series_federated_local.SeriesFederatedLocal
     """
-    sample_0_numpy = np.array(
-        [17.2, 20.9, 22.6, 18.1, 21.7, 21.4, 23.5, 24.2, 14.7, 21.8]
-    )
+    sample_0_numpy = np.array([17.2, 20.9, 22.6, 18.1, 21.7, 21.4, 23.5, 24.2, 14.7, 21.8])
     sample_1_numpy = np.array(
         [
             21.5,
