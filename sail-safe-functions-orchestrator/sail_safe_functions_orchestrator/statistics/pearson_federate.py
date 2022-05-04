12cfd7037127277148b78a7d2c2285bf1d1c869b
NeuralMonkSail <saurabh@secureailabs.com>
2022-05-04 13:57:24 +0000

    BOARD-1492 Added pearson

---
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/statistics/pearson_federate.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+from sail_safe_functions_orchestrator.series_federated import SeriesFederated
+
+from sail_safe_functions.statistics.pearson_agregate import PearsonAgregate
+from sail_safe_functions.statistics.pearson_precompute import PearsonPrecompute
+
+
+class PearsonFederate:
+    @staticmethod
+    def pearson(
+        sample_0: SeriesFederated,
+        sample_1: SeriesFederated,
+    ) -> Tuple[float, float]:
+
+        list_list_precompute = []
+        list_key_dataframe = list(sample_0.dict_series.keys())
+        # TODO deal with posibilty sample_0 and sample_1 do net share same child frames
+        for key_dataframe in list_key_dataframe:
+            list_list_precompute.append(
+                PearsonPrecompute.run(
+                    sample_0.dict_series[key_dataframe],
+                    sample_1.dict_series[key_dataframe],
+                )
+            )
+        r = PearsonAgregate.run(list_list_precompute)
+
+        return r
---
+++ sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/test_statistics/pearson_test.py
@@ -0,0 +1,24 @@
+import pytest
+from scipy import stats
+from typing import Tuple
+from sail_safe_functions_orchestrator.statistics.pearson_federate import PearsonFederate
+from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import SeriesFederatedLocal
+
+
+@pytest.mark.active
+def test_pearson(two_sample_big: Tuple[SeriesFederatedLocal, SeriesFederatedLocal]):
+    """
+    This test our federated Pearson module
+
+    """
+    # Arrange
+    sample_0 = two_sample_big[0]
+    sample_1 = two_sample_big[1]
+
+    # Calling fedrated sail Pearson method
+    pearson_sail = PearsonFederate.pearson(sample_0, sample_1)
+
+    # Act
+    pearson_scipy, p = stats.pearsonr(sample_0.to_numpy(), sample_1.to_numpy())
+    # Assert
+    assert pearson_scipy == pytest.approx(pearson_sail, 0.0001)
