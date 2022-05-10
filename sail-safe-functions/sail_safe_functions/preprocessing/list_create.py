390073b738b955020efca841a2448c92dabfcab1
Jaap Oosterbroek <jaap@secureailabs.com>
2022-05-10 16:46:13 +0200

    added two safe functions

---
+++ sail-safe-functions/sail_safe_functions/preprocessing/list_create.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class ListCreate:
+    """
+    Creates an empty list for aggregation
+    """
+
+    def Run() -> List:
+        """Creates an empty list for aggregation
+
+        :return: The created list
+        :rtype: List
+        """
+        return []
