9be8360d33ae9854c7f3bbcf09f3b997b61e0ed1
Jaap Oosterbroek <jaap@secureailabs.com>
2022-05-11 15:34:10 +0200

    Added homogenious execution, and reference calls

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/statistics/mean_federate.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/statistics/mean_federate.py
@@ -1,3 +1,5 @@
+import numpy
+import pandas
 from sail_safe_functions.statistics.mean_agregate import MeanAgregate
 from sail_safe_functions.statistics.mean_precompute import MeanPrecompute
 from sail_safe_functions_orchestrator.series_federated import SeriesFederated
@@ -14,3 +16,11 @@ class MeanFederate:
         mean_statistic = MeanAgregate.run(list_list_precompute)
 
         return mean_statistic
+
+    @staticmethod
+    def Run(sample_0: SeriesFederated):
+        return MeanFederate.mean(sample_0)
+
+    @staticmethod
+    def RunReference(sample_0: pandas.Series):
+        return numpy.mean(sample_0.to_numpy())
