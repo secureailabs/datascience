dc8fe5938a713d5ccdefbfafe32643996d1b768c
Jaap Oosterbroek <jaap@secureailabs.com>
2022-05-14 10:15:02 +0200

    Test min max working

---
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/statistics/min_max_federate.py
@@ -0,0 +1,41 @@
+from typing import Tuple
+
+import numpy
+import pandas
+from sail_safe_functions.statistics.min_max_agregate import MinMaxAgregate
+from sail_safe_functions.statistics.min_max_precompute import MinMaxPrecompute
+from sail_safe_functions_orchestrator.series_federated import SeriesFederated
+from scipy import stats
+
+
+class MinMaxFederate:
+    """
+    Class that wraps the safe function for min and max
+
+    """
+
+    @staticmethod
+    def min_max(
+        sample_0: SeriesFederated,
+    ) -> Tuple[float, float]:
+
+        list_list_precompute = []
+        # TODO deal with posibilty sample_0 and sample_1 do net share same child frames
+
+        # Calculating precompute
+        for series in sample_0.dict_series.values():  # TODO remplace these
+            list_list_precompute.append(MinMaxPrecompute.run(series))
+
+        # Final min max values
+        min, max = MinMaxAgregate.run(list_list_precompute)
+        return min, max
+
+    @staticmethod
+    def Run(sample_0: SeriesFederated):
+        return MinMaxFederate.min_max(sample_0)
+
+    @staticmethod
+    def RunReference(sample_0: pandas.Series):
+        min_numpy = numpy.min(sample_0.to_numpy())
+        max_numpy = numpy.max(sample_0.to_numpy())
+        return min_numpy, max_numpy
---
+++ sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/test_statistics/min_max_test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+from sail_safe_functions_orchestrator.statistics.min_max_federate import MinMaxFederate
+from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import SeriesFederatedLocal
+from scipy import stats
+
+
+@pytest.mark.active
+def test_min_max(one_sample_big: SeriesFederatedLocal):
+    """This test our federated Skewness module
+
+    :param one_sample_big: A single federated series fixture
+    :type one_sample_big: SeriesFederatedLocal
+    """
+
+    # Arrange
+    sample_0 = one_sample_big
+
+    # Act
+    # Calling fedrated sail skewness method
+    min_sail, max_sail = MinMaxFederate.Run(sample_0)
+    min_numpy, max_numpy = MinMaxFederate.RunReference(sample_0)
+
+    # Assert
+    assert max_numpy <= max_sail
+    assert min_numpy >= min_sail
