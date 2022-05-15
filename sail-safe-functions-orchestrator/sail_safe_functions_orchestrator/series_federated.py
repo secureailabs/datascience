a89ac8766c857d1a8039f2ed26a777033a281397
Jaap Oosterbroek <jaap@secureailabs.com>
2022-05-15 12:35:04 +0200

    both spearmand and pearson upgraded

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/series_federated.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/series_federated.py
@@ -4,6 +4,8 @@ from abc import ABC
 class SeriesFederated(ABC):
     def __init__(self) -> None:
         self._dict_series = {}
+        self.name = None
+        self.size = 0
 
     def items(self):
         return self._dict_series.items()
