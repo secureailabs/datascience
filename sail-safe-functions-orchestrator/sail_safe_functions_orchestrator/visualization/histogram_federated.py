b2fec582d471e2eeb420dd30ee8aac281a8e3ac1
NeuralMonkSail <saurabh@secureailabs.com>
2022-06-09 12:42:41 +0000

    added plotly

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/visualization/histogram_federated.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/visualization/histogram_federated.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 from sail_safe_functions.visualization.histogram_agregate import HistogramAgregate
 from sail_safe_functions.visualization.histogram_precompute import HistogramPrecompute
 from sail_safe_functions_orchestrator.series_federated import SeriesFederated
@@ -5,16 +6,16 @@ from sail_safe_functions_orchestrator.series_federated import SeriesFederated
 
 class HistogramFederate:
     @staticmethod
-    def hist(sample_0: SeriesFederated):
+    def hist(sample_0: SeriesFederated, bin_count: int):
 
         list_list_precompute = []
         for series in sample_0.dict_series.values():
             list_list_precompute.append(HistogramPrecompute.run(series))
 
         hist_value = HistogramAgregate.run(list_list_precompute)
-
-        return hist_value
+        fig = px.histogram(hist_value, nbins=bin_count)
+        return fig
 
     @staticmethod
-    def Run(sample_0: SeriesFederated):
-        return HistogramFederate.hist(sample_0)
+    def Run(sample_0: SeriesFederated, bin_count: int):
+        return HistogramFederate.hist(sample_0, bin_count)
