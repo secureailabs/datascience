0bbc38cf77fbd3d957ef0014933b3f01aac3b9ae
Jaap Oosterbroek <jaap@secureailabs.com>
2022-08-02 14:07:29 +0300

    added general demo BOARD-1761

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/preprocessing/cdf.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/preprocessing/cdf.py
@@ -1,12 +1,8 @@
 from typing import List, Tuple
 
 import numpy
-from sail_safe_functions.preprocessing.cdf_aggregate import (
-    CumulativeDistributionFunctionAggregate,
-)
-from sail_safe_functions.preprocessing.cdf_precompute import (
-    CumulativeDistributionFunctionPrecompute,
-)
+from sail_safe_functions.preprocessing.cdf_aggregate import CumulativeDistributionFunctionAggregate
+from sail_safe_functions.preprocessing.cdf_precompute import CumulativeDistributionFunctionPrecompute
 from sail_safe_functions_orchestrator.series_federated import SeriesFederated
 from sail_safe_functions_orchestrator.statistics.min_max import MinMax
 
@@ -29,26 +25,17 @@ class CumulativeDistributionFunction:
     @staticmethod
     def run(sample_0: SeriesFederated) -> Tuple[List[float], List[float]]:
 
-        domain_min, domain_max = MinMax.min_max(sample_0)
-
+        domain_min, domain_max = MinMax().run(sample_0)
         list_precompute = []
         for series in sample_0.dict_series.values():  # TODO rework abcs
-            list_precompute.append(
-                CumulativeDistributionFunctionPrecompute.run(
-                    series, domain_min, domain_max
-                )
-            )
-        return CumulativeDistributionFunctionAggregate.run(
-            list_precompute, domain_min, domain_max
-        )
+            list_precompute.append(CumulativeDistributionFunctionPrecompute.run(series, domain_min, domain_max))
+        return CumulativeDistributionFunctionAggregate.run(list_precompute, domain_min, domain_max)
 
     def run_reference(sample_0: SeriesFederated) -> Tuple[List[float], List[float]]:
         array_sample_0 = sample_0.to_numpy()
         array_domain = numpy.sort(array_sample_0)
         array_domain = numpy.insert(array_domain, 0, array_domain[0])
 
-        array_value = numpy.arange(1, len(array_sample_0) + 1) / float(
-            len(array_sample_0)
-        )
+        array_value = numpy.arange(1, len(array_sample_0) + 1) / float(len(array_sample_0))
         array_value = numpy.insert(array_value, 0, 0)
         return array_domain.tolist(), array_value.tolist()
--- sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/test_statistics/min_max_test.py
+++ sail-safe-functions-test/sail_safe_functions_test/test_sail_safe_functions/test_statistics/min_max_test.py
@@ -1,13 +1,14 @@
+from typing import Tuple
+
 import pytest
 from sail_safe_functions_orchestrator.statistics.min_max import MinMax
-from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import (
-    SeriesFederatedLocal,
-)
+from sail_safe_functions_test.helper_sail_safe_functions.data_frame_federated_local import DataFrameFederatedLocal
+from sail_safe_functions_test.helper_sail_safe_functions.series_federated_local import SeriesFederatedLocal
 
 
 @pytest.mark.active
 def test_min_max(one_sample_big: SeriesFederatedLocal):
-    """This test our federated Skewness module
+    """This test our federated min max function
 
     :param one_sample_big: A single federated series fixture
     :type one_sample_big: SeriesFederatedLocal
@@ -24,3 +25,24 @@ def test_min_max(one_sample_big: SeriesFederatedLocal):
     # Assert
     assert max_numpy <= max_sail
     assert min_numpy >= min_sail
+
+
+@pytest.mark.active
+def test_min_max_kidney(tuple_kidney_schema_dataframe: Tuple[dict, DataFrameFederatedLocal]):
+    """This test our federated min max function with the clean kidney dataset
+
+    :param one_sample_big: A single federated data_frame_federated_kidney fixture
+    :type one_sample_big: SeriesFederatedLocal
+    """
+
+    # Arrange
+    sample_0 = tuple_kidney_schema_dataframe[1]["age"]
+
+    # Act
+    estimator = MinMax()
+    min_sail, max_sail = estimator.run(sample_0)
+    min_numpy, max_numpy = estimator.run_reference(sample_0)
+
+    # Assert
+    assert min_numpy >= min_sail
+    assert max_numpy <= max_sail
