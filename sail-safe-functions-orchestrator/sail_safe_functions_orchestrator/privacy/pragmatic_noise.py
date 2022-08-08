95a6b8a0fb1af5b30df58ea3f28a7f16010ea98d
Stanley <stanley@secureailabs.com>
2022-08-08 09:54:18 -0400

    Fixing lint errors for prgmatic_noise func

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/privacy/pragmatic_noise.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/privacy/pragmatic_noise.py
@@ -6,7 +6,19 @@ from sail_safe_functions_orchestrator.tools_common import check_instance
 
 
 def pragmatic_noise(data_frame_source: DataFrameFederated, query_expresion: Any) -> DataFrameFederated:
-    return Query.run(data_frame_source, query_expresion)
+    """
+    > The function `pragmatic_noise` takes a `DataFrameFederated` and a query expression and returns a
+    `DataFrameFederated`
+
+    :param data_frame_source: DataFrameFederated
+    :type data_frame_source: DataFrameFederated
+    :param query_expresion: Any
+    :type query_expresion: Any
+    :return: A DataFrameFederated object
+    """
+    # A placeholder for the query expression.
+    # return Query.run(data_frame_source, query_expresion)
+    pass
 
 
 class PragmaticNoise:
