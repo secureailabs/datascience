780bc6ed57f8eb2978a0805798cd22c21f12376f
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2022-08-10 17:12:40 +0100

    BOARD-1020 Initial Design complete

--- sail-safe-functions/sail_safe_functions/machine_learning/models/LogisticRegression.py
+++ sail-safe-functions/sail_safe_functions/machine_learning/models/LogisticRegression.py
@@ -5,7 +5,7 @@ class LogisticRegression(torch.nn.Module):
     # TODO: Add more parameters for model
     def __init__(self, inputSize, outputSize, bias=False):
         super(LogisticRegression, self).__init__()
-        self.model = torch.nn.Linear(inputSize, outputSize, bias=False)
+        self.model = torch.nn.Linear(inputSize, outputSize, bias)
 
     def forward(self, x):
         out = self.model(x)
