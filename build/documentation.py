a5ec9d8d0fb22b31663c09abed90e7cc5f8037a3
Stanley <stanley@secureailabs.com>
2022-08-31 12:24:18 -0400

    adding a wait to documentation.py

--- build/documentation.py
+++ build/documentation.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import time
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 print(f"Script Directory: {script_dir}")
@@ -13,3 +14,6 @@ subprocess.run(["pdoc", "sail_safe_functions", "--html", "-o", "../docs"])
 subprocess.run(["pdoc", "sail_safe_functions_orchestrator", "--html", "-o", "../docs"])
 subprocess.run(["pdoc", "sail_safe_functions_test", "--html", "-o", "../docs"])
 shutil.copyfile("index.html.bak", "../docs/index.html")
+
+time.sleep(3)  # Sleep for 3 seconds
+print(f"Documentation generated in docs/")
