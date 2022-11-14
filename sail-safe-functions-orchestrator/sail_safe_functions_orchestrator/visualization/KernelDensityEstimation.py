1111f545fc526d8cc3a1c7dd65c402c652b438b7
NeuralMonkSail <saurabh@secureailabs.com>
2022-11-14 08:28:53 +0000

    updated the changes

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/visualization/KernelDensityEstimation.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/visualization/KernelDensityEstimation.py
@@ -1,12 +1,17 @@
 import plotly.figure_factory as ff
 from sail_safe_functions.visualization.kde_aggregate import KdeAggregate
 from sail_safe_functions.visualization.kde_precompute import KdePrecompute
-from sail_safe_functions_orchestrator.series_federated import SeriesFederated
+from sail_safe_functions_orchestrator.series import Series
 
 
-class Kde:
+class KernelDensityEstimation_class:
+    """
+    Performs the federated kernel density estimation.
+
+    """
+
     @staticmethod
-    def kde(sample_0: SeriesFederated, group_labels: str, bin_size: float):
+    def KernelDensityEstimation(sample_0: Series, group_labels: str, bin_size: float):
         """
         Performs the federated kernel density estimation.
         It take on federated series and bin count. Returns the kde plot.
@@ -20,13 +25,19 @@ class Kde:
 
         """
         list_list_precompute = []
-        for series in sample_0.dict_series.values():
-            list_list_precompute.append(KdePrecompute.run(series))
+        for dataset_id in sample_0.list_dataset_id:
+            client = sample_0.service_client.get_client(dataset_id)
+            list_list_precompute.append(
+                client.call(
+                    KdePrecompute,
+                    sample_0.dict_reference_series[dataset_id],
+                )
+            )
 
         kde_value = KdeAggregate.run(list_list_precompute)
         fig = ff.create_distplot(kde_value, group_labels, bin_size)
         return fig
 
     @staticmethod
-    def run(sample_0: SeriesFederated, group_labels: str, bin_size: float):
-        return Kde.kde(sample_0, group_labels, bin_size)
+    def run(sample_0: Series, group_labels: str, bin_size: float):
+        return KernelDensityEstimation_class.KernelDensityEstimation(sample_0, group_labels, bin_size)
