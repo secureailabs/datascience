8ff2d6823c4268b0dbe31bdc05a440a1686fb363
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2022-11-29 19:26:58 +0800

    BOARD-2049 commented out levene and drop_na on series

--- fastapi/smart_broker_api/preprocessing_api.py
+++ fastapi/smart_broker_api/preprocessing_api.py
@@ -15,15 +15,16 @@ def drop_na_data_frame(session, data_frame_id):
     return result.json()["result_data_frame_id"]
 
 
-def drop_na_series(session, series_id):
-    payload = {"series_id": series_id}
-    result = requests.post(
-        "http://"
-        + session.ip
-        + ":"
-        + session.port
-        + "/preprocessing/series/drop_missing/"
-        + series_id,
-        params=payload,
-    )
-    return result.json()["series_id"]
+## Doesn't work but we should allou it
+# def drop_na_series(session, series_id):
+#     payload = {"series_id": series_id}
+#     result = requests.post(
+#         "http://"
+#         + session.ip
+#         + ":"
+#         + session.port
+#         + "/preprocessing/series/drop_missing/"
+#         + series_id,
+#         params=payload,
+#     )
+#     return result.json()["series_id"]
