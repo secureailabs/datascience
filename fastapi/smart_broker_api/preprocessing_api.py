f2c336525778993707453833060494d41e5e3f0a
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2022-11-30 19:46:15 +0800

    BOARD-2049 Fix Linting

--- fastapi/smart_broker_api/preprocessing_api.py
+++ fastapi/smart_broker_api/preprocessing_api.py
@@ -4,18 +4,13 @@ import requests
 def drop_na_data_frame(session, data_frame_id):
     payload = {"data_frame_id": data_frame_id}
     result = requests.post(
-        "http://"
-        + session.ip
-        + ":"
-        + session.port
-        + "/preprocessing/data_frame/drop_missing/"
-        + data_frame_id,
+        "http://" + session.ip + ":" + session.port + "/preprocessing/data_frame/drop_missing/" + data_frame_id,
         params=payload,
     )
     return result.json()["result_data_frame_id"]
 
 
-## Doesn't work but we should allou it
+# NOTE: Doesn't work but we should allow it
 # def drop_na_series(session, series_id):
 #     payload = {"series_id": series_id}
 #     result = requests.post(
