b1d91400dfca928c5de916a8a93b9c2db406a0d2
NeuralMonkSail <saurabh@secureailabs.com>
2022-12-06 12:34:22 +0000

    fixing lint

--- sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/transform/scaling_min_max.py
+++ sail-safe-functions-orchestrator/sail_safe_functions_orchestrator/transform/scaling_min_max.py
@@ -1,11 +1,7 @@
 from typing import List
 
-import numpy
-from sail_safe_functions_orchestrator import transform
-from sail_safe_functions_orchestrator.data_frame_federated import \
-    DataFrameFederated
-from sail_safe_functions_orchestrator.transform.scaling_quantile import \
-    ScalingQuantile
+from sail_safe_functions_orchestrator.data_frame_federated import DataFrameFederated
+from sail_safe_functions_orchestrator.transform.scaling_quantile import ScalingQuantile
 
 
 class ScalingMinMax:
