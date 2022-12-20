302701df961b4e9445d07bc8173c0e213fc7119d
David-G-SAIL <92735760+David-G-SAIL@users.noreply.github.com>
2022-12-20 11:50:16 -0500

    BOARD 2102 - Integrated Smart Broker, local notebooks and client from MVP demo (#81)
    * [BOARD-2033] [BOARD-2045]
    work on federation aspect
    added unit test using logitudinal dataset
    added PATH_DIR_DATASET to docker
    added a t-test
    added a big fhir dataset
    converting to tabular remotely
    Safe function super class
    Add missing init files
    added more integration tests
    
    * fixed some regresion erros
    
    * Add a test specifically to integrate with dock SCN deployments
    
    * Add a .gitignore
    
    * Test scripts for a rest API with DS
    
    * Added REST endpoints
    
    * BOARD-2049 Now working from series
    
    * BOARD-2047 up to student t test
    
    * BOARD-2047 statistics complete with errors
    
    * BOARD-2047 Validation working
    
    * BOARD-2047 Update notebook
    
    * BOARD-2047 Added API modules
    
    * BOARD-2047 Dynamic IP allocation
    
    * BOARD-2047 preprocessing endpoints added
    
    * BOARD-2047 commented out series na
    
    * BOARD-2049 Everything but levene
    
    * BOARD-2049 commented out levene and drop_na on series
    
    * BOARD-2049 create series model added but on the fritz
    
    * BOARD-2049 Levene test working
    
    * BOARD-2049 Linting issues
    
    * BOARD-2049 Fix Linting
    
    * First pass of working with IVs
    
    * Working integration with IVs
    
    * Updates from today's demo
    
    * Updated notebook
    
    * azure deployment
    
    * BOARD-2114 changes for azure
    
    * BOARD-2102 Merge Visualizations
    
    * BOARD-2102 Notebook from today's early demo
    
    * BOARD-2102 Allow an ENV variable for IV in smart broker
    
    * Bringing over the platform API
    
    * Tidying up a bit
    
    * Notebooks and APIs for provisions and stats
    
    * Add notebooks for provision demo
    
    * Add ssl flag for session objects
    
    Move session objects to their own file
    
    * Add missing use of session object
    
    * Notebooks from this morning's demo
    
    * Remove un-needed file
    
    * Fixes from review
    
    Co-authored-by: Jaap Oosterbroek <jaap@secureailabs.com>
    Co-authored-by: AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
    Co-authored-by: Prawal Gangwar <prawal.gangwar@gmail.com>

--- fastapi/smart_broker_api/preprocessing_api.py
+++ fastapi/smart_broker_api/preprocessing_api.py
@@ -4,8 +4,19 @@ import requests
 def drop_na_data_frame(session, data_frame_id):
     payload = {"data_frame_id": data_frame_id}
     result = requests.post(
-        "http://" + session.ip + ":" + session.port + "/preprocessing/data_frame/drop_missing/" + data_frame_id,
+        session.get_url() + "/preprocessing/data_frame/drop_missing/" + data_frame_id,
         params=payload,
+        verify=False,
+    )
+    return result.json()["result_data_frame_id"]
+
+
+def query_data_frame(session, data_frame_id, query):
+    payload = {"query_str": query}
+    result = requests.post(
+        session.get_url() + "/preprocessing/data_frame/query/" + data_frame_id,
+        params=payload,
+        verify=False,
     )
     return result.json()["result_data_frame_id"]
 
