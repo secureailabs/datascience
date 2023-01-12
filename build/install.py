7d6f34f86e8bf1df686b777f3eed3f536a85bf7b
JaapOosterbroek <jaap@secureailabs.com>
2023-01-12 08:46:27 -0500

    minus nl

--- build/install.py
+++ build/install.py
@@ -3,4 +3,3 @@ import subprocess
 subprocess.run(["pip", "install", "-e", "sail-core"])
 subprocess.run(["pip", "install", "-e", "sail-safe-functions"])
 subprocess.run(["pip", "install", "-e", "sail-aggregator-fastapi"])
-
