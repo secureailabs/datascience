102db6cddbb36cc06fa5f3753498c8fdf6d6f057
JaapOosterbroek <jaap@secureailabs.com>
2023-01-19 08:08:31 -0500

    added the private key

--- Docker/sail-scn-docker/deploy.py
+++ Docker/sail-scn-docker/deploy.py
@@ -1,8 +1,7 @@
-docker run -p hostPort:containerPort imageName
+# docker run -p hostPort:containerPort imageName
 
 
-
-    # docker run --it --entrypoint=bash pklehre/niso2020-lab2-msc
+# docker run --it --entrypoint=bash pklehre/niso2020-lab2-msc
 
 
 import os
@@ -14,6 +13,7 @@ import sys
 # export PATH_FILE_GIT_TOKEN
 # python deploy.py BOARD-2078-B BOARD-2078
 def run(command: str):
+    print(command)
     subprocess.call(command)
 
 
@@ -22,11 +22,31 @@ if __name__ == "__main__":
 
     # remove images
 
-    name_data_federation: str = sys.argv[1]
+    # name_data_federation: str = sys.argv[1]
+    path_dir_dataset = os.environ["PATH_DIR_DATASET"]
+    path_dir_public_key_zeromq = os.environ["PATH_DIR_PUBLIC_KEY_ZEROMQ"]
+    path_file_private_key_zeromq = os.environ["PATH_FILE_PRIVATE_KEY_ZEROMQ_SERVER"]
 
     command: str = "docker run"
+    command += " -it"
+    command += f" --mount src={path_dir_public_key_zeromq},target=/app/RPCLib/public_keys/,type=bind"
+    command += (
+        f" --mount src={path_file_private_key_zeromq},target=/app/RPCLib/private_keys/server.key_secret,type=bind"
+    )
+    command += f" --mount src={path_dir_dataset},target=/data/,type=bind"
 
-    command += f" -t sail/im_scn ."
-    command += f" -p hostPort:containerPort"
+    command += " -e PATH_DIR_PUBLIC_KEY_ZEROMQ=/app/RPCLib/public_keys/"
+    command += " -e PATH_FILE_PRIVATE_KEY_ZEROMQ_SERVER=/app/RPCLib/private_keys/server.key_secret"
 
+    command += " -e PATH_DIR_DATASET=/data/"
+
+    command += " -p 5010:5010"
+    command += " --entrypoint=/entrypoint_participant.sh"
+    command += " sail/im_scn ."
     run(command)
+
+
+# export PATH_DIR_PUBLIC_KEY_ZEROMQ="/c/key/public_key_zeromq"
+# export PATH_FILE_PRIVATE_KEY_ZEROMQ_SERVER="/c/key/private_key_zeromq/server.key_secret"
+
+# /app/RPCLib/public_keys/#
--- Docker/sail-scn-docker/entrypoint_participant.sh
+++ Docker/sail-scn-docker/entrypoint_participant.sh
@@ -5,9 +5,5 @@
 #nginx -g 'daemon off;' 2>&1 | tee /app/nginx.log &
 
 # Start the Public API Server
-export PATH_DIR_PUBLIC_KEY_ZEROMQ="/app/RPCLib/public_keys/"
-export PATH_FILE_PRIVATE_KEY_ZEROMQ_CLIENT="/app/RPCLib/private_keys/client.key_secret"
-export PATH_DIR_DATASET="/data/"
-
 /bin/python3.8 /datascience/sail-participant-zeromq/server.py 5010
 
