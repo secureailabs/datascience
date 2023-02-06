8585893665e3eaaebc8d00644e858bae44a0e643
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2023-02-06 16:40:12 +0700

    BOARD-2292 data API converted, need data to test

--- sail-aggregator-fastapi/smart_broker_api/data_api.py
+++ sail-aggregator-fastapi/smart_broker_api/data_api.py
@@ -1,80 +1,59 @@
 from typing import List
 
 import requests
+from fast_api_client.models import (
+    BodyDataFrameSelectSeries,
+    BodyDataFrameTabularSelectDataframe,
+    BodyDatasetTabularFhirv1,
+    BodyReadDatasetTabularFromLongitudinal,
+    DataFederation,
+)
 
 
-def read_longitudinal_fhirv1(session):
-    result = requests.post(
-        session.get_url() + "/ingestion/read_longitudinal/fhirv1",
-        params=None,
-        verify=False,
-    )
-    return result.json()["longitudinal_id"]
+def read_longitudinal_fhirv1(operation):
+
+    return operation.read_longitudinal_fhirv1().additional_properties["longitudinal_id"]
 
 
-def parse_dataset_tabular_from_longitudinal(
-    session,
+def read_dataset_tabular_from_longitudinal(
+    operation,
     longitudinal_id,
     dataset_federation_id,
     dataset_federation_name,
     data_model_tabular_id,
 ):
-    payload = {
-        "longitudinal_id": longitudinal_id,
-        "dataset_federation_id": dataset_federation_id,
-        "dataset_federation_name": dataset_federation_name,
-        "data_model_tabular_id": data_model_tabular_id,
-    }
-    result = requests.post(
-        session.get_url() + "/ingestion/read_dataset_tabular_from_longitudinal",
-        params=payload,
-        verify=False,
+
+    body = BodyReadDatasetTabularFromLongitudinal(
+        longitudinal_id, dataset_federation_id, dataset_federation_name, data_model_tabular_id
     )
-    return result.json()["dataset_id"]
+
+    return operation.read_dataset_tabular_from_longitudinal(body).additional_properties["dataset_id"]
 
 
-def parse_dataset_tabular_from_fhirv1(session, dataset_federation_id, dataset_federation_name, data_model_tabular_id):
+# TODO: add 'read' to operation_id
+def dataset_tabular_fhirv1(operation, dataset_federation_id, dataset_federation_name, data_model_tabular_id):
 
-    payload = {
-        "dataset_federation_id": dataset_federation_id,
-        "dataset_federation_name": dataset_federation_name,
-        "data_model_tabular_id": data_model_tabular_id,
-    }
+    body = BodyDatasetTabularFhirv1(dataset_federation_id, dataset_federation_name, data_model_tabular_id)
 
-    result = requests.post(session.get_url() + "/ingestion/dataset_tabular/fhirv1", params=payload, verify=False)
-    return result.json()["dataset_id"]
+    return operation.dataset_tabular_fhirv1(body).additional_properties["tabular_dataframe_id"]
 
 
-def data_frame_tabular_select_data_frame(session, data_frame_tabular_id, data_frame_name):
-    payload = {
-        "data_frame_tabular_id": data_frame_tabular_id,
-        "data_frame_name": data_frame_name,
-    }
-    result = requests.post(
-        session.get_url() + "/data_frame_tabular/select_dataframe/" + data_frame_tabular_id,
-        params=payload,
-        verify=False,
-    )
-    return result.json()["data_frame_id"]
+def data_frame_tabular_select_data_frame(operation, data_frame_tabular_id, data_frame_name):
+
+    body = BodyDataFrameTabularSelectDataframe(data_frame_tabular_id, data_frame_name)
+
+    return operation.data_frame_tabular_select_dataframe(body).additional_properties["data_frame_id"]
 
 
-def data_frame_select_series(session, data_frame_id, series_name):
-    payload = {"data_frame_id": data_frame_id, "series_name": series_name}
-    result = requests.post(
-        session.get_url() + "/data_frame/select_series/" + data_frame_id,
-        params=payload,
-        verify=False,
-    )
-    return result.json()["series_id"]
+def data_frame_select_series(operation, data_frame_id, series_name):
+
+    body = BodyDataFrameSelectSeries(data_frame_id, series_name)
+
+    return operation.data_frame_select_series(body).additonal_properties["series_id"]
 
 
-def read_dataset_csvv1(session, list_dataset_id: List[str]):
-    payload = {
-        "list_dataset_id": list_dataset_id,
-    }
-    result = requests.post(
-        session.get_url() + "/ingestion/read_dataset_csvv1",
-        json=payload,
-        verify=False,
-    )
-    return result.json()["dataset_id"]
+def read_tabular_dataframe_csvv1(operation, list_dataset_id: List[str]):
+
+    body = DataFederation(list_dataset_id)
+
+    return operation.read_tabular_dataframe_csvv1(body)["dataset_id"]
