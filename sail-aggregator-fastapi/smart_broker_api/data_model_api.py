5f84e0f6f2f20cdc7d428fa2b187b7cca6d709fb
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2023-02-06 15:49:29 +0700

    BOARD-2292 Data Model API converted

--- sail-aggregator-fastapi/smart_broker_api/data_model_api.py
+++ sail-aggregator-fastapi/smart_broker_api/data_model_api.py
@@ -1,55 +1,33 @@
 import requests
+from fast_api_client.models import (
+    BodyDataframeModelAddNewSeriesModel,
+    BodyNewSeriesModelNumerical,
+    BodyTabularModelAddDataframeModel,
+)
 
 
-def create_date_frame(session, data_frame_name):
-    payload = {"data_frame_name": data_frame_name}
-    result = requests.post(session.get_url() + "/data_model/new_data_frame_model", params=payload, verify=False)
-    return result.json()["data_model_id"]
+def new_data_model_data_frame(operation, data_frame_name):
+    return operation.new_data_model_data_frame(data_frame_name).additional_properties["data_model_id"]
 
 
-def create_tabular_data(session):
-    result = requests.post(session.get_url() + "/data_model/new_tabular_model", params=None, verify=False)
-    return result.json()["data_frame_tabular_id"]
+def new_tabular_model(operation):
+    return operation.new_tabular_model().additional_properties["data_frame_tabular_id"]
 
 
-def create_series(session, series_name, measurement_source_name, type_aggregator, unit):
-    payload = {
-        "series_name": series_name,
-        "measurement_source_name": measurement_source_name,
-        "type_aggregator": type_aggregator,
-        "unit": unit,
-    }
-    result = requests.post(
-        session.get_url() + "/data_model/new_series_model_numerical",
-        params=payload,
-        verify=False,
+def new_series_model_numerical(operation, series_name, measurement_source_name, type_aggregator, unit):
+    body = BodyNewSeriesModelNumerical(series_name, measurement_source_name, type_aggregator, unit)
+    # TODO: change series to series_id
+    return operation.new_series_model_numerical(body).additional_properties["series"]
+
+
+def data_frame_add_series(operation, dataframe_model_id, series_name, measurement_source_name, type_agregator, unit):
+    body = BodyDataframeModelAddNewSeriesModel(
+        dataframe_model_id, series_name, measurement_source_name, type_agregator, unit
     )
-    return result.json()["series"]
+
+    return operation.dataframe_model_add_new_series_model(body).additional_properties["data_model_id"]
 
 
-def data_frame_add_series(session, data_model_id, series_name, measurement_source_name, type_agregator):
-    payload = {
-        "data_model_id": data_model_id,
-        "series_name": series_name,
-        "measurement_source_name": measurement_source_name,
-        "type_agregator": type_agregator,
-    }
-    result = requests.post(
-        session.get_url() + "/data_model/data_frame/" + data_model_id + "/add_new_series_model",
-        params=payload,
-        verify=False,
-    )
-    return result.json()["data_model_id"]
-
-
-def tabular_add_dataframe(session, data_model_dataframe_id, data_model_tabular_id):
-    payload = {
-        "data_model_dataframe_id": data_model_dataframe_id,
-        "data_model_tabular_id": data_model_tabular_id,
-    }
-    result = requests.post(
-        session.get_url() + "/data_model/tabular/" + data_model_tabular_id + "/add_dataframe",
-        params=payload,
-        verify=False,
-    )
-    return result.json()["data_frame_tabular_id"]
+def tabular_model_add_dataframe_model(operation, data_model_tabular_id, data_model_dataframe_id):
+    body = BodyTabularModelAddDataframeModel(data_model_tabular_id, data_model_dataframe_id)
+    return operation.tabular_model_add_dataframe_model(body).additional_properties["data_frame_tabular_id"]
