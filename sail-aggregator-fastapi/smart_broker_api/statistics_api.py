b37daa1fd719060c7df94186af1291a5785cfcc1
AdamHallSAIL <91956585+AdamHallSAIL@users.noreply.github.com>
2023-02-06 23:31:31 +0700

    BOARD-2292 stats done

--- sail-aggregator-fastapi/smart_broker_api/statistics_api.py
+++ sail-aggregator-fastapi/smart_broker_api/statistics_api.py
@@ -1,6 +1,18 @@
 from ast import literal_eval
 
 import requests
+from fast_api_client.models import (
+    BodyMannWhitneyUTest,
+    BodyStatisticsChisquare,
+    BodyStatisticsKolmogorovSmirnovTest,
+    BodyStatisticsLeveneTest,
+    BodyStatisticsPairedTTest,
+    BodyStatisticsPearson,
+    BodyStatisticsSpearman,
+    BodyStatisticsStudentTTest,
+    BodyStatisticsWelchTTest,
+    BodyStatisticsWilcoxonSignedRankTest,
+)
 
 # def interpret_result(result):
 #     if result.status_code == 200:
@@ -13,173 +25,105 @@ import requests
 
 
 def count(operation, series_id):
-    return operation.statistics_count(series_id)
+    return operation.statistics_count(series_id).additional_properties["count"]
 
 
-def mean(session, series_id):
-    payload = {"series_id": series_id}
-    result = requests.post(session.get_url() + "/statistics/mean/" + series_id, params=payload, verify=False)
-    return interpret_result(result)
+def mean(operation, series_id):
+    return operation.statistics_mean(series_id).additional_properties["mean"]
 
 
-def chisquare(session, series_1_id, series_2_id):
-    payload = {"series_1_id": series_1_id, "series_2_id": series_2_id}
-    result = requests.post(
-        session.get_url() + "/statistics/chisquare/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def chisquare(operation, series_1_id, series_2_id):
+    body = BodyStatisticsChisquare(series_1_id, series_2_id)
+    return operation.statistics_chisquare(body).additional_properties["chisquare"]
 
 
-def kolmogorovSmirnovTest(session, series_1_id, type_distribution, type_ranking):
-    payload = {
-        "series_1_id": series_1_id,
-        "type_distribution": type_distribution,
-        "type_ranking": type_ranking,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/kolmogorovSmirnovTest/" + series_1_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def kolmogorov_smirnov_test(operation, series_1_id, type_distribution):
+
+    body = BodyStatisticsKolmogorovSmirnovTest(series_1_id, type_distribution)
+    return operation.statistics_kolmogorov_smirnov_test(body).additional_properties["kolmogorov_smirnov_test"]
 
 
-def kurtosis(session, series_id):
-    payload = {"series_id": series_id}
-    result = requests.post(session.get_url() + "/statistics/kurtosis/" + series_id, params=payload, verify=False)
-    return interpret_result(result)
+def kurtosis(operation, series_id):
+    return operation.statistics_kurtosis(series_id).additional_properties["kurtosis"]
 
 
-def levene_test(session, series_1_id, series_2_id):
-    payload = {"series_1_id": series_1_id, "series_2_id": series_2_id}
-    result = requests.post(
-        session.get_url() + "/statistics/levene_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def levene_test(operation, series_1_id, series_2_id):
+
+    body = BodyStatisticsLeveneTest(series_1_id, series_2_id)
+
+    result = operation.statistics_levene_test(body).additional_properties
+
+    return result["f_statistic"], result["p_value"]
 
 
-def mann_whitney_u_test(session, series_1_id, series_2_id, alternative, type_ranking):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-        "type_ranking": type_ranking,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/mann_whitney_u_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
-
-
-def min_max(session, series_1_id):
-    payload = {"series_id": series_1_id}
-    result = requests.post(
-        session.get_url() + "/statistics/min_max/" + series_1_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
-
-
-def paired_t_test(session, series_1_id, series_2_id, alternative):
+def mann_whitney_u_test(operation, series_1_id, series_2_id, alternative):
     payload = {
         "series_1_id": series_1_id,
         "series_2_id": series_2_id,
         "alternative": alternative,
     }
-    result = requests.post(
-        session.get_url() + "/statistics/paired_t_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+
+    body = BodyMannWhitneyUTest(series_1_id, series_2_id, alternative)
+    result = operation.mann_whitney_u_test(body).additional_properties
+
+    return result["w_statistic"], result["p_value"]
 
 
-def pearson(session, series_1_id, series_2_id, alternative):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/pearson/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def min_max(operation, series_1_id):
+
+    return operation.statistics_min_max(series_1_id)
 
 
-def skewness(session, series_id):
-    payload = {"series_id": series_id}
-    result = requests.post(session.get_url() + "/statistics/skewness/" + series_id, params=payload, verify=False)
-    return interpret_result(result)
+def paired_t_test(operation, series_1_id, series_2_id, alternative):
+
+    body = BodyStatisticsPairedTTest(series_1_id, series_2_id, alternative)
+
+    result = operation.statistics_paired_t_test(body).additional_properties
+
+    return result["t_statistic"], result["p_value"]
 
 
-def spearman(session, series_1_id, series_2_id, alternative, type_ranking):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-        "type_ranking": type_ranking,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/spearman/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def pearson(operation, series_1_id, series_2_id, alternative):
+
+    body = BodyStatisticsPearson(series_1_id, series_2_id, alternative)
+
+    result = operation.statistics_pearson(body).additional_properties
+
+    return result["pearson"], result["p_value"]
 
 
-def student_t_test(session, series_1_id, series_2_id, alternative):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/student_t_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def skewness(operation, series_id):
+
+    return operation.statistics_skewness(series_id).additional_properties["skewness"]
 
 
-def variance(session, series_id):
-    payload = {"series_id": series_id}
-    result = requests.post(session.get_url() + "/statistics/variance/" + series_id, params=payload, verify=False)
-    return interpret_result(result)
+def spearman(operation, series_1_id, series_2_id, alternative):
+
+    body = BodyStatisticsSpearman(series_1_id, series_2_id, alternative)
+    result = operation.statistics_spearman(body)
+
+    return result["spearman"], result["p_value"]
 
 
-def welch_t_test(session, series_1_id, series_2_id, alternative):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/welch_t_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def student_t_test(operation, series_1_id, series_2_id, alternative):
+
+    body = BodyStatisticsStudentTTest(series_1_id, series_2_id, alternative)
+    result = operation.statistics_student_t_test(body).additional_properties
+
+    return result["t_statistic"], result["p_value"]
 
 
-def wilcoxon_signed_rank_test(session, series_1_id, series_2_id, alternative, type_ranking):
-    payload = {
-        "series_1_id": series_1_id,
-        "series_2_id": series_2_id,
-        "alternative": alternative,
-        "type_ranking": type_ranking,
-    }
-    result = requests.post(
-        session.get_url() + "/statistics/wilcoxon_signed_rank_test/" + series_1_id + "/" + series_2_id,
-        params=payload,
-        verify=False,
-    )
-    return interpret_result(result)
+def variance(operation, series_id):
+    return operation.statistics_variance(series_id)
+
+
+def welch_t_test(operation, series_1_id, series_2_id, alternative):
+    body = BodyStatisticsWelchTTest(series_1_id, series_2_id, alternative)
+    result = operation.statistics_welch_t_test(body).additional_properties
+    return result["t_statistic"], result["p_value"]
+
+
+def wilcoxon_signed_rank_test(operation, series_1_id, series_2_id, alternative):
+    body = BodyStatisticsWilcoxonSignedRankTest(series_1_id, series_2_id, alternative)
+    result = operation.statistics_wilcoxon_signed_rank_test(body)
+    return result["w_statistic"], result["p_value"]
