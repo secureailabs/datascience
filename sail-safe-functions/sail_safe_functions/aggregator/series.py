9f2886706c3c04690ec8d776252bd5a622e9e0b8
NeuralMonkSail <saurabh@secureailabs.com>
2023-02-14 15:10:23 +0000

    incomplete lots of bug

--- sail-safe-functions/sail_safe_functions/aggregator/series.py
+++ sail-safe-functions/sail_safe_functions/aggregator/series.py
@@ -1,15 +1,15 @@
 from typing import List
 
+import numpy as np
 from pandas import Series as SeriesPandas
 from sail_safe_functions.aggregator.data_model.data_model_series import DataModelSeries
 
 
-class Series(SeriesPandas):
+class Series:
     # NOTE Long term this overloading trick is not maintainable and we will need to create a
     # class where the pandas object in a member not a superclass
     # TODO Put the datamodel last because we do that everwhere else
     def __init__(self, dataset_id: str, data_model_series: DataModelSeries, list_data: List) -> None:
-        super().__init__(data=list_data, name=data_model_series.series_name)
         # TODO trying to avoid circular import here solution would be to move the reference but that is invasive
         if not isinstance(dataset_id, str):
             raise Exception(f"{dataset_id} is not instance of class: {str} instead type is {type(dataset_id)}")
@@ -19,10 +19,24 @@ class Series(SeriesPandas):
             )
         self._dataset_id = dataset_id
         self._data_model_series = data_model_series
+        self.data = list_data
+        self.name = data_model_series.series_name
+        self.series_pandas = SeriesPandas(data=list_data, name=data_model_series.series_name)
+        self.index = SeriesPandas.index
+        self.size = SeriesPandas.size
 
     @property
     def series_name(self) -> str:
         return self._data_model_series.series_name
+
+    def to_numpy(self) -> np.ndarray:
+        return self.series_pandas.to_numpy()
+
+    def value_counts(self):
+        return self.series_pandas.value_counts()
+
+    def to_list(self):
+        return self.series_pandas.to_list()
 
     @property
     def dataset_id(self) -> str:
